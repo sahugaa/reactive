9406a0d76a9ae40c4fdc882dd4c46ff3df758fd9
Ganesh Sahu <sahuliferay@gmail.com>
2023-02-17 13:45:18 +0530

    Add files via upload

---
+++ asyncio/src/main/java/com/aysnc/asyncio/AsyncRestApplication.java
@@ -0,0 +1,13 @@
+package com.rest.async.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncRestApplication.class, args);
+	}
+
+}
